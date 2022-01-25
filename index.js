c3045fa188d0aa80656675e6dc8f7e8eaea647bc
Sultan UL Arefin <programmer.arefin@gmail.com>
2022-01-25 16:56:52 +0600

    update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use('/', require('./_controller/request.controller'));
 // global error handler
 app.use(errorHandler);
 
-// start server
+// start server from developer account
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
